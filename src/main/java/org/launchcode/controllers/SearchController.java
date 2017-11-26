24225936e5800ecff2f67685851d50978d8cd6c6
paul <paulvrobson@gmail.com>
2017-11-26 01:43:48 -0500

    Complete TODO 1 -add capability to search by search term

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -16,12 +17,19 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+    private ArrayList<HashMap<String, String>> jobs;
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", this.jobs);
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
-
+    @RequestMapping(value = "/results", method = RequestMethod.POST)
+    public String searchByType(@RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        this.jobs = jobs;
+        return "redirect:";
+    }
 }
